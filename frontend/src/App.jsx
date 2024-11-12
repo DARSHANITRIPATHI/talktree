f3618a1a03eb25af4ca0673ff9e96b8593859565
DARSHANITRIPATHI <darshanitripathi0000@gmail.com>
2024-11-12 20:45:59 +0530

    socket completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import './App.css';
 
 import Home from './pages/home/Home';
+import { Navigate,Routes,Route } from 'react-router-dom';
+import Login from './pages/login/Login';
+
+import { Toaster } from 'react-hot-toast';
+import SignUp from './pages/signup/SignUp';
+import { useAuthContext } from './context/AuthContext';
+
 
 const App = () => {
+  const {authUser} =useAuthContext();
   return (
-    <div className='p-4 h-screen flex items-center justify-center'>
-        {/* <Signup/> */}
-        <Home/>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={authUser ?<Home /> :<Navigate to={"/login"}/>} />
+        <Route path="/login" element={authUser ? <Navigate to ='/' /> :<Login />} />
+        <Route path="/signup" element={authUser ? <Navigate to ='/' /> :<SignUp/>} />
+      </Routes>
+      <Toaster/>
     </div>
   );
 }
 
-export default App
+export default App;
